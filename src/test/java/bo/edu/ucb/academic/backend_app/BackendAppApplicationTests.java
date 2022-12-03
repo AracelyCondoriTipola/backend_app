d51d72520f4baba4836d07c6bc9a0a66f47b948c
AracelyCondoriTipola <aracely96.ac>
2022-12-03 05:32:07 -0400

    primer commit

---
+++ src/test/java/bo/edu/ucb/academic/backend_app/BackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.academic.backend_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
